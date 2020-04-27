a64ae301511683beff76120a2a23cb92fecd9012
wangxuanzhong <wangxuanzhong@rcplatformhk.com>
2020-04-27 10:58:32 +0800

    子序列

---
+++ src/main/java/arithmetic/EnumerateSubSet.java
@@ -0,0 +1,46 @@
+package arithmetic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个没有重复数字的序列，返回其所有可能的全排列。
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+public class EnumerateSubSet {
+
+    /**
+     * 循环枚举
+     */
+    public static List<List<Integer>> enumerate(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        for (Integer n : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newSub = new ArrayList<Integer>(res.get(i));
+                newSub.add(n);
+                res.add(newSub);
+            }
+        }
+        return res;
+    }
+
+}
